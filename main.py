ead14e743bfaeeecd934ec9c0a14105bda7b5a3f
jo-project <jo.project.0911@gmail.com>
2023-07-26 14:47:34 +1000

    Leet Code (Python)

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import argparse
 import importlib
 import os
-from rich import print
 import re
 import sys
 import io
+import time
+
+pprint = print
+from rich import print
+from rich.progress import track
+from rich.panel import Panel
+from rich.markdown import Markdown
+from utils import superscript, subscript
 
 def get_input_comment(problem_file):
     with open(problem_file, "r") as f:
@@ -37,18 +44,30 @@ def get_category_from_file(problem_folder):
 
 def get_question_from_file(problem_folder):
     info_file = os.path.join(problem_folder, "info.txt")
-    if os.path.isfile(info_file):
-        with open(info_file, "r") as f:
-            found_question = False
-            question_lines = []
-            for line in f:
-                if found_question:
-                    question_lines.append(line.strip())
-                elif line.strip().startswith("Question:"):
-                    found_question = True
-                    question_lines.append(line.strip().replace("Question:", "").strip())
-            return "\n".join(question_lines)
-    return None
+    if not os.path.isfile(info_file):
+        return None
+
+    with open(info_file, "r") as f:
+        found_question = False
+        question_lines = []
+        for line in f:
+            if found_question or line.strip().startswith("Question:"):
+                line = re.sub(r'<sub>(.*?)<\/sub>', lambda match: subscript(match.group(1)), line.strip())
+                line = re.sub(r'<sup>(.*?)<\/sup>', lambda match: superscript(match.group(1)), line)
+                question_lines.append(line.replace("Question:", "").strip())
+                found_question = True
+
+    return "\n".join(question_lines)
+
+
+def loading(time_needed: int):
+    for _ in track(range(time_needed), description="Compiling..."):
+        time.sleep(1)
+
+    CURSOR_UP_ONE = '\x1b[1A'
+    ERASE_LINE = '\x1b[2K'
+    pprint(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE)
+
 
 def run_problem(problem_number):
     problem_folder = f"problems/problem{problem_number}"
@@ -58,28 +77,6 @@ def run_problem(problem_number):
         print(f"[bold red]Error:[/bold red] Problem file 'problem{problem_number}.py' not found.")
         return None
 
-    title = get_title_from_file(problem_folder)
-    if title:
-        print(f"[b #89b4fa]Problem {problem_number}[/b #89b4fa] │ {title}")
-    else:
-        print(f"[b #89b4fa]Problem {problem_number}[/b #89b4fa]")
-
-    print("[blink]────────────────────────────────────────[/blink]")
-
-    category = get_category_from_file(problem_folder)
-    if category:
-        print(f"[b #cba6f7]Category:[/b #cba6f7] [#cdd6f4]{category}[/#cdd6f4]")
-    else:
-        print(f"[b #cba6f7]Category:[/b #cba6f7] [#cdd6f4]None[/#cdd6f4]")
-
-    question = get_question_from_file(problem_folder)
-    if question:
-        print(f"[b #f38ba8]Question:[/b #f38ba8] [#cdd6f4]{question}[/#cdd6f4]")
-
-    input_comment = get_input_comment(problem_file)
-    if input_comment:
-        print(f"[b #b4befe]Input:[/b #b4befe] [#cdd6f4]{input_comment}[/#cdd6f4]")
-
     # Capture the standard output
     old_stdout = sys.stdout
     sys.stdout = io.StringIO()
@@ -88,12 +85,34 @@ def run_problem(problem_number):
     module.main()
 
     # Get the captured output
+    title = get_title_from_file(problem_folder)
+    category = get_category_from_file(problem_folder)
+    question = get_question_from_file(problem_folder)
+    input = get_input_comment(problem_file)
     output = sys.stdout.getvalue().strip()
 
     # Restore the standard output
     sys.stdout = old_stdout
+    if title:
+        print(f"[b #89b4fa]Problem {problem_number}[/b #89b4fa] │ [b #ffffff]{title}[/b #ffffff]")
+    else:
+        print(f"[b #89b4fa]Problem {problem_number}[/b #89b4fa]")
 
-    print(f"[b #a6e3a1]Output:[/b #a6e3a1] {output}", end=" ")
+    print("[blink]────────────────────────────────────────[/blink]")
+
+    if category:
+        print(f"[b #cba6f7]Category:[/b #cba6f7] [#cdd6f4]{category}[/#cdd6f4]")
+    else:
+        print(f"[b #cba6f7]Category:[/b #cba6f7] [#cdd6f4]None[/#cdd6f4]")
+
+    if question:
+        question = Markdown(question)
+        print(Panel(question, title="[b #f38ba8]Question[/b #f38ba8]"))
+
+    if input:
+        print(Panel(input, title="[b #b4befe]Input[/b #b4befe]"))
+
+    print(Panel(output, title="[b #a6e3a1]Output[/b #a6e3a1]", highlight=True), end=" ")
 
     return output
 
@@ -102,7 +121,7 @@ def main():
     parser = argparse.ArgumentParser(description="Run LeetCode Problems")
     parser.add_argument("-P", "--problem", type=int, required=True, help="Problem number to run")
     args = parser.parse_args()
-
+    loading(2)
     run_problem(args.problem)
 
     # print("\n[bold green]Finished![/bold green]")
